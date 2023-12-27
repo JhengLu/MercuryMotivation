b47a56e798a4c4c3b78b56a3a104df4574aaa42d
jiahenglu <jiahengluuu@gmail.com>
2023-12-26 20:48:47 -0500

    fix [bug] for  the calculation

--- createCommandlab-GB.py
+++ createCommandlab-GB.py
@@ -7,19 +7,19 @@ isolate the node 1 as the remote memory
 '''
 
 if __name__ == '__main__':
-    sysReserveCap = 2 * 1024
-    WSSCapicaty = 3*1024  # the WSS
-    localDRAM_endLocation = int(504 * 1024)
-    remoteDRAM_capacity = 80 * 1024
-    totalDRAM_capacity = 504 * 1024 * 2
+    sysReserveCap = 2
+    WSSCapicaty = 143  # the WSS
+    localDRAM_endLocation = int(512)
+    remoteDRAM_capacity = 400
+    totalDRAM_capacity = 512 * 2
     remoteDRAM_stopLocation = localDRAM_endLocation + remoteDRAM_capacity
     remoteDRAM_leftcapacity = totalDRAM_capacity - remoteDRAM_stopLocation
     for ratio in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]: # set the local DRAM according to the ratio of WSS
         localDRAM_Capacity = int(ratio * WSSCapicaty)
-        localDRAM_stopLocation = int(0 + sysReserveCap + WSSCapicaty)
+        localDRAM_stopLocation = int(0 + sysReserveCap + localDRAM_Capacity)
         localDRAM_leftCapacity = localDRAM_endLocation - localDRAM_stopLocation
-        currentCommand = f'''GRUB_CMDLINE_LINUX="isolcpus=28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111 memmap={localDRAM_leftCapacity}M!{localDRAM_stopLocation}M memmap={remoteDRAM_leftcapacity}M!{remoteDRAM_stopLocation}M" '''
-        print("current capacity: " + str(localDRAM_Capacity)+ " MB")
+        currentCommand = f'''GRUB_CMDLINE_LINUX="isolcpus=28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111 memmap={localDRAM_leftCapacity}G!{localDRAM_stopLocation}G memmap={remoteDRAM_leftcapacity}G!{remoteDRAM_stopLocation}M" '''
+        print("current capacity: " + str(localDRAM_Capacity)+ " GB")
         print("ratio of WSS:" + str(ratio))
         print(currentCommand)
         print()
